485c48e1111a649cd9bfe039454488f4ba27da2f
DaniBoi <Danny@dynamic-10-125-213-191.dynamic.unt.edu>
2018-02-24 18:10:48 -0600

    added navigation bar with buttons sending to different activities

--- app/src/main/java/com/example/maceo/babylog/FeedingActivity.java
+++ app/src/main/java/com/example/maceo/babylog/FeedingActivity.java
@@ -2,12 +2,17 @@ package com.example.maceo.babylog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class FeedingActivity extends AppCompatActivity {
+public class FeedingActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeding);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
--- app/src/main/java/com/example/maceo/babylog/HomeActivity.java
+++ app/src/main/java/com/example/maceo/babylog/HomeActivity.java
@@ -1,30 +1,64 @@
 package com.example.maceo.babylog;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     // TODO: Declare member variables here:
     Button mFeedingButton;
     Button mSleepButton;
     Button mDiaperChangeButton;
     Button mChartButtton;
+    FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout myDrawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        ActionBarDrawerToggle myToggle = new ActionBarDrawerToggle(this,myDrawer,toolbar,R.string.open,R.string.close);
+
+        myDrawer.addDrawerListener(myToggle);
+        myToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         // TODO: casting we converted a view object to a button object
         mFeedingButton = (Button) findViewById(R.id.feeding_button);
         mSleepButton = (Button) findViewById(R.id.sleep_button);
         mDiaperChangeButton = (Button) findViewById(R.id.diaper_change_button);
         mChartButtton = (Button) findViewById(R.id.chart_button);
+
+        fab = (FloatingActionButton)findViewById(R.id.fab1);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(HomeActivity.this, "Fab Clicked", Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         // TODO: set OnClick listener to change screen when button is pressed
         //example
@@ -68,4 +102,50 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.feed) {
+            startActivity(new Intent(this,FeedingActivity.class));
+
+        } else if (id == R.id.diaper) {
+            startActivity(new Intent(this,DiaperChangeActivity.class));
+
+        } else if (id == R.id.sleep) {
+            startActivity(new Intent(this,SleepActivity.class));
+
+        } else if (id == R.id.chart) {
+            startActivity(new Intent(this,ChartActivity.class));
+
+        } else if (id == R.id.journal) {
+//            startActivity(new Intent(this,.class));
+
+        } else if (id == R.id.tt) {
+//            startActivity(new Intent(this,.class));
+
+        } else if (id == R.id.weightT) {
+//            startActivity(new Intent(this,.class));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+
 }
