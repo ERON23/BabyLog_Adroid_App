20975e841ecde397529739a7170096014d0a8540
Danny <salas.danny2013@gmail.com>
2018-02-25 20:20:23 -0600

    Added functionality to the floating action button and added the camera feature
    Signed-off-by: Danny <salas.danny2013@gmail.com>

--- app/src/main/java/com/example/maceo/babylog/HomeActivity.java
+++ app/src/main/java/com/example/maceo/babylog/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.maceo.babylog;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -12,29 +14,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionMenu;
 
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
+    // TODO: Declare member variables here:
+    // for FAB
+    FloatingActionMenu floatingActionMenu;
+    com.github.clans.fab.FloatingActionButton camera, sleep, feeding;
+    // for fragment pages
     private PagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
-
-    // TODO: Declare member variables here:
-    FloatingActionButton fab;
+    // for camera
+    private static final int CAM_REQUEST=1313;
+    ImageView imgTakenPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // finds FAB menu
+        floatingActionMenu = (FloatingActionMenu)findViewById(R.id.floatingActionMenu);
+        camera = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.fab_camera);
+        sleep = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.fab_sleep);
+        feeding = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.fab_food);
+
+        /* start of individual button implementation */
+        feeding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionMenu.toggle(true);
+                Intent intent= new Intent(HomeActivity.this,FeedingActivity.class);
+                startActivity(intent);
+            }
+        });
+        camera.setOnClickListener(new btnTakePhotoClicker());
+        sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionMenu.toggle(false);
+                Intent intent= new Intent(HomeActivity.this,SleepActivity.class);
+                startActivity(intent);
+            }
+        });
+        /* end of individual button implementation */
+
+        // sets up PagerAdapter Class
         mPagerAdapter = new PagerAdapter(getSupportFragmentManager());
         // Setup the ViewPager with the sections adapter
         mViewPager = (ViewPager) findViewById(R.id.pager);
         setupViewPager(mViewPager);
 
+        /* start of the tab layout and navigation view */
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(mViewPager);
 
@@ -49,18 +88,7 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        fab = (FloatingActionButton)findViewById(R.id.fab1);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(HomeActivity.this, "FAB Clicked", Toast.LENGTH_LONG).show();
-
-            }
-        });
-
+        /* start of the tab layout and navigation view */
     }
 
     private void setupViewPager(ViewPager viewPager){
@@ -86,7 +114,6 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        //here is the main place where we need to work on.
         int id=item.getItemId();
         switch (id){
             case R.id.home:
@@ -126,5 +153,26 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // Do Not delete
+    // this will show picture on the app but taken off for now
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAM_REQUEST){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            imgTakenPic.setImageBitmap(bitmap);
+        }
+    }*/
+
+    // sends request to take pic
+    class btnTakePhotoClicker implements com.github.clans.fab.FloatingActionButton.OnClickListener{
+        @Override
+        public void onClick(View view){
+            floatingActionMenu.toggle(false);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAM_REQUEST);
+        }
     }
 }
