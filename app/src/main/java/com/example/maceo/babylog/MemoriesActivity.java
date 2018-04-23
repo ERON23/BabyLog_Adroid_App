63d262f0f18ef931218f5097a8b8b172aa7c2a1d
DaniBoi <danny@dynamic-10-125-213-191.dynamic.unt.edu>
2018-04-23 12:15:31 -0500

    shows images now
    Signed-off-by: Danny <salas.danny2013@gmail.com>

--- app/src/main/java/com/example/maceo/babylog/MemoriesActivity.java
+++ app/src/main/java/com/example/maceo/babylog/MemoriesActivity.java
@@ -3,10 +3,10 @@ package com.example.maceo.babylog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -147,7 +147,7 @@ public class MemoriesActivity extends AppCompatActivity {
                                     taskSnapshot.getDownloadUrl().toString());
                             String uploadId = mDatabaseRef.push().getKey();
                             newPost.put("name", mEditTextFileName.getText().toString().trim());
-                            newPost.put("uploadId", taskSnapshot.getDownloadUrl().toString());
+                            newPost.put("imageUrl", taskSnapshot.getDownloadUrl().toString());
                             mDatabaseRef.child(uploadId).setValue(newPost);
                         }
                     })
